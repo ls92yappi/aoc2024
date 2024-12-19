5f6da667ad388bc975f1d93516a02a55918b8e33
ls92yappi <ls92@cindatasci.com>
2024-12-19 11:05:34 -0500

    Simplified template

--- probTemplate.go
+++ probTemplate.go
@@ -1,16 +1,14 @@
 package main
 
 // See README.md for problem description
-
 // Usage: problem1 input1.txt
-// Currently templated off of my aoc2023/day23 code
 
 import (
 	"fmt"
 	//"regexp"
 	//"strconv"
 	//"strings"
-	. "github.com/ls92yappi/aoc" // problems with go proxy not being even remotely up-to-date
+	. "github.com/ls92yappi/aoc"
 )
 
 ///////////////////////////////////////////////////////////
@@ -20,14 +18,12 @@ import (
 
 
 func processInput(fname string) int {
-	// n is both the number of lines and the square grid size
 	lines, n, _ := ReadWholeFile(fname)
 	answer := 0
 
 	for i := range(n) {
 		line := lines[i]
 		//rule,num,err := IntSlice(line,"|")
-		//AllRules = append(AllRules, rule)
 		_ = line
 	} // for i
 
@@ -38,7 +34,6 @@ func processInput(fname string) int {
 }
 
 func main() {
-	//AllRules = make([][]int, 0)
 	inputFileName := InputFileName()
 	output := processInput(inputFileName)
 	fmt.Println(output)
